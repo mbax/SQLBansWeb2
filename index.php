b06b0dbf5590dd1d6463654c21790d81267fd1f7
Dane Everitt <dane@daneeveritt.com>
2012-07-15 21:37:47 -0400

    Begin Session handling

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -25,37 +28,18 @@
       </div><!-- /navbar -->
     </section>
     <div class="container">
-		<div class="row">
-			<div class="span12">
-				<div class="alert alert-error">
-			       		<h4 class="alert-heading">403 - Access Denied</h4>
-			        	<p>This section is only avaliable to registered members. Please login to continue.</p>
-				</div>
-			</div>
-		</div>
+		<?php
 		
-		<form class="form-horizontal well">
-			<fieldset>
-				<legend>Login to Appeal Ban</legend>
-		        	<div class="control-group">
-		            	<label class="control-label">Username</label>
-		            		<div class="controls">
-		              			<input type="text" class="input-xlarge" name="username">
-		              				<p class="help-block">Please input your Minecraft username exactly as it is spelled including capitalization and underscores.</p>
-		            		</div>
-		          	</div>
-		          	<div class="control-group">
-		          		<label class="control-label">Unique Ban ID</label>
-		          			<div class="controls">
-		          	  			<input type="text" class="input-xlarge" name="banid">
-		          	  				<p class="help-block">Enter the Ban ID found in your ban reason.</p>
-		          			</div>
-		          	</div>
-		          	<div class="form-actions">
-		            	<button type="submit" class="btn btn-primary">Login</button>
-		          	</div>
-			</fieldset>
-		</form>
+			/*
+			 * Show Login Page
+			 */
+			if(!isset($_SESSION['sqlbans_user']) && !isset($_SESSION['sqlbans_user_ip'])){
+				include_once('pages/login.php');
+			}else if($_SESSION['sqlbans_user_ip'] != $_SERVER['REMOTE_ADDR']){
+				include_once('pages/login.php');
+			}
+		
+		?>
 		
       <footer class="footer">
         <p class="pull-right">Copyright &copy; 2012 - SQLBans Team</p>
---
+++ pages/login.php
@@ -0,0 +1,30 @@
+<div class="row">
+	<div class="span12">
+		<div class="alert alert-error">
+	       		<h4 class="alert-heading">403 - Access Denied</h4>
+	        	<p>This section is only avaliable to registered members. Please login to continue.</p>
+		</div>
+	</div>
+</div>
+<form class="form-horizontal well">
+	<fieldset>
+		<legend>Login to Appeal Ban</legend>
+        	<div class="control-group">
+            	<label class="control-label">Username</label>
+            		<div class="controls">
+              			<input type="text" class="input-xlarge" name="username">
+              				<p class="help-block">Please input your Minecraft username exactly as it is spelled including capitalization and underscores.</p>
+            		</div>
+          	</div>
+          	<div class="control-group">
+          		<label class="control-label">Unique Ban ID</label>
+          			<div class="controls">
+          	  			<input type="text" class="input-xlarge" name="banid">
+          	  				<p class="help-block">Enter the Ban ID found in your ban reason.</p>
+          			</div>
+          	</div>
+          	<div class="form-actions">
+            	<button type="submit" class="btn btn-primary">Login</button>
+          	</div>
+	</fieldset>
+</form>
